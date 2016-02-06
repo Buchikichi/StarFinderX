4579a3f18fc2591b0366a60c482f7f0ccf07f3c4
ぽん太 <ponta@macintosh.local>
2016-02-06 11:08:47 +0900

    Initial Commit

---
+++ StarFinderX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarFinderX
+//
+//  Created by ぽん太 on 2016/02/06.
+//  Copyright © 2016年 Nekoko Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
